4de086b8e6968e5dc7d1ad4e12a5782c78a3f72f
Aarav Solanki <aaravsolanki1704@gmail.com>
2019-03-09 14:36:34 -0500

    Moved all TCNJ files into this repo

---
+++ extension/popup.js
@@ -0,0 +1,10 @@
+console.log("TEST");
+$(function() {
+    $('enable_punishment').click(function() {
+        console.log("Enable Punishment");
+    });
+
+    $('disable_punishment').click(function() {
+        console.log("Disable Punishment");
+    });
+});
