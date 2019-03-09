6cfdfa0016b96330c1ad96c2ec8ae8e7bf0ee2e6
KingLeoFenil <leofenil1234@gmail.com>
2019-03-09 16:07:41 -0500

    changed

--- extension/background.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-console.log("TSEST");
-=======
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://turnthatsmileupsidedown.com/";
-  chrome.tabs.create({ url: newURL });
-});
->>>>>>> 416570cf0fc142467c1ae5514919a2c6c9e70f9d
