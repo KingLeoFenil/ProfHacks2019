196316b2ce9a81678009a3d8f3ac28ff268c3f47
KingLeoFenil <leofenil1234@gmail.com>
2019-03-10 06:14:36 -0400

    js

--- js/init.js
+++ js/init.js
@@ -7,15 +7,28 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+// The following example creates complex markers to indicate beaches near
+// Sydney, NSW, Australia. Note that the anchor is set to (0,32) to correspond
+// to the base of the flagpole.
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: -33.9, lng: 151.2}
+  });
+
+  setMarkers(map);
+}
+
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
-var clams = [
+var beaches = [
   ['Bondi Beach', -33.890542, 151.274856, 4],
   ['Coogee Beach', -33.923036, 151.259052, 5],
   ['Cronulla Beach', -34.028249, 151.157507, 3],
   ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-  ['Maroubra Bea ch', -33.950198, 151.259302, 1]
+  ['Maroubra Beach', -33.950198, 151.259302, 1]
 ];
 
 function setMarkers(map) {
